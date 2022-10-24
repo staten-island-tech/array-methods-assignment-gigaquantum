0a5fa2aba9dc138374b8442e53c07f00960126da
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-10-24 13:15:30 -0400

    array

---
+++ app.js
@@ -0,0 +1,11 @@
+const array = [
+  { letter: "alpha", numbers: [2, 4, 6, 8], float: 2.9, iceCream: "Vanilla" },
+  { letter: "beta", numbers: [3, 6, 9, 12], float: 8.6, iceCream: "Chocolate" },
+  { letter: "gamma", numbers: [1, 2, 4, 8], float: 4.8, iceCream: "Stawberry" },
+  {
+    letter: "delta",
+    numbers: [2.5, -2.5, 5.5, -5.5],
+    float: 9.1,
+    iceCream: "Mint",
+  },
+];
