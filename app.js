9b0e1e16f43dc228abc4c4f2ed3c6e5d2ea15ada
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-10-24 21:20:09 -0400

    html fixed

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const array = [
-  { letter: "alpha", numbers: [2, 4, 6, 8], float: 2.9, iceCream: "Vanilla" },
-  { letter: "beta", numbers: [3, 6, 9, 12], float: 8.6, iceCream: "Chocolate" },
-  { letter: "gamma", numbers: [1, 2, 4, 8], float: 4.8, iceCream: "Stawberry" },
+  { name: "alpha", numbers: [2, 4, 6, 8], float: 2.9, iceCream: "Vanilla" },
+  { name: "beta", numbers: [3, 6, 9, 12], float: 8.6, iceCream: "Chocolate" },
+  { name: "gamma", numbers: [1, 2, 4, 8], float: 4.8, iceCream: "Stawberry" },
   {
-    letter: "delta",
+    name: "delta",
     numbers: [2.5, -2.5, 5.5, -5.5],
     float: 9.1,
     iceCream: "Mint",
   },
 ];
+
+array.forEach((obj) => {
+  console.log(obj);
+});
