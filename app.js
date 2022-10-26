305718f9ff068e2a53bdd7edcea05aa2b225ee21
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-10-26 13:14:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const array = [
+const arr = [
   { name: "alpha", numbers: [2, 4, 6, 8], float: 2.9, iceCream: "Vanilla" },
-  { name: "beta", numbers: "[3, 6, 9, 12]", float: 8.6, iceCream: "Chocolate" },
+  { name: "beta", numbers: [3, 6, 9, 12], float: 8.6, iceCream: "Chocolate" },
   { name: "gamma", numbers: [1, 2, 4, 8], float: 4.8, iceCream: "Stawberry" },
   {
     name: "delta",
@@ -10,4 +10,18 @@ const array = [
   },
 ];
 
-array.forEach((element, index) => console.log(element, name, index));
+arr.forEach(object => {
+  console.log(object.name);
+});
+
+arr.forEach(object => {
+  object.numbers.forEach(numArr => {
+    console.log(numArr);
+  });
+});
+
+const newArr = arr.filter(object => {
+  object.float.value > 5;
+});
+
+console.log(newArr);
